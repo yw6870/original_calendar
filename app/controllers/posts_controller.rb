5f1fefd9491c06dfc2f390be0b5be678662beb33
yw6870 <68420090+yw6870@users.noreply.github.com>
2021-01-28 01:44:10 +0900

    修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,13 +13,10 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @post.destroy
     @posts = Post.all.order("created_at DESC")
-    respond_to do |format|
-      format.js
-    end
   end
 
   private
   def post_params
-    params.require(:post).permit(:title, :body).merge(user_id: current_user.id)
+    params.require(:post).permit(:title).merge(user_id: current_user.id)
   end
 end
