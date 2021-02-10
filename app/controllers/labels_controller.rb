9d8f489ea5c80a730e6e800dc611c02dde31d25c
yw6870 <68420090+yw6870@users.noreply.github.com>
2021-02-10 15:22:28 +0900

    コントローラー作成

---
+++ app/controllers/labels_controller.rb
@@ -0,0 +1,13 @@
+class LabelsController < ApplicationController
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'labels/resources'
   devise_for :users
   root to: 'events#index'
   resources :events
   resources :posts
+  resources :labels
 end
